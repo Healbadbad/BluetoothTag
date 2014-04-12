3a0e55d2b698daf93724a14c5627d958b97a8a4c
chinchalupa <chinchalupa@gmail.com>
2014-04-12 15:04:03 -0400

    Added buttons to homepage.

--- src/com/example/bluetoothtag/MainActivity.java
+++ src/com/example/bluetoothtag/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -19,6 +20,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void sendGameRequest(View view) {
+	}
+	
+	public void findGameRequest(View view) {
+		
 	}
 
 }
