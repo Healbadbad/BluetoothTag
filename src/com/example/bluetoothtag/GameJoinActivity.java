3dc4fd24f1bbf6f6c2d0af72f0f292fc79229d37
Healbadbad <healbadbad@yahoo.com>
2014-04-13 10:05:07 -0400

    doing stuff

--- src/com/example/bluetoothtag/GameJoinActivity.java
+++ src/com/example/bluetoothtag/GameJoinActivity.java
@@ -2,26 +2,12 @@ package com.example.bluetoothtag;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class GameJoinActivity extends Activity {
-
-	ListView joinList;
-	
-	ArrayAdapter<String> adapter;
-	
-	@Override
+public class GameJoinActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.gamejoiner);
 		
-		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-		
-		joinList = (ListView) findViewById(R.id.joinlist);
-		joinList.setAdapter(adapter);
 		
 	}
-
 }
--- src/com/example/bluetoothtag/GameLobbyActivity.java
+++ src/com/example/bluetoothtag/GameLobbyActivity.java
@@ -2,25 +2,33 @@ package com.example.bluetoothtag;
 
 import java.util.HashMap;
 
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
-public class GameHostActivity extends Activity{
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class GameLobbyActivity extends Activity {
+
+	ListView joinList;
+	
+	ArrayAdapter<String> adapter;
 	static HashMap<String,HashMap<String,String>> Firedata = new HashMap<String,HashMap<String,String>>();
-	ArrayAdapter adapter;
+	ArrayAdapter adapter2;
 	private BluetoothAdapter bluetoothAdapter;
 	String[] players;
+	String hostname;
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gamejoiner);
 		final String selfname = bluetoothAdapter.getDefaultAdapter().getName();
 		Firebase ref = new Firebase("https://blistering-fire-1807.firebaseio.com/");
 		ref.addValueEventListener(new ValueEventListener() {
@@ -36,19 +44,19 @@ public class GameHostActivity extends Activity{
 			}
 		});
 		HashMap<String,String> data = new HashMap<String,String>();
-		data.put("Lobby",selfname);
+//		data.put("Lobby",selfname);
+		data.put("Lobby",Firedata.get(hostname).get("Lobby"));
 		data.put("IT","");
 		Firedata.put(selfname, data);
 		players = Firedata.get(selfname).get("Lobby").split(":");
 		adapter = new ArrayAdapter(this, R.layout.activity_list_item, players);
 		ListView lv = new ListView(this);
 		lv.setAdapter(adapter);// maybe want to add a click handler to kick people here?
-		
 		this.setContentView(lv);// Want it to display lobby, so need to start lobby with current host, blank lobby, blank it, need startgame button
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,players);
+		joinList = (ListView) findViewById(R.id.joinlist);
+		joinList.setAdapter(adapter);
 		
 	}
-	private void startGame(){
-		Intent intent = new Intent(this, GamePlayingActivity.class);
-		startActivity(intent);
-	}
+
 }
